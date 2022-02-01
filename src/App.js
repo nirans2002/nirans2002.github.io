f73c44cf0d431996eb8b50ac1208cfee416f4f56
Niran S Narayanan <nirans2002@gmail.com>
2022-02-01 16:26:25 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button, Alert,Breadcrumb,Card} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+       
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +20,18 @@ function App() {
         >
           Learn React
         </a>
+        <Alert variant="success">Button here</Alert>
+        <Button className="">TEST</Button>
+        <Card style={{color: 'black'}}>
+          <Card.Header>Header</Card.Header>
+          <Card.Img></Card.Img>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>example react bootstrap</Card.Text>
+          </Card.Body>
+        </Card>
+
+
       </header>
     </div>
   );

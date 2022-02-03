72bfcbcb6266372a8f07033339484f002613c446
Niran S Narayanan <nirans2002@gmail.com>
2022-02-03 15:28:45 +0530

    nav color

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -7,15 +7,14 @@ export const NavigationBar = () => {
   return <div>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="#home">Niran S Narayanan</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="ms-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#about">Projects</Nav.Link>
-      <Nav.Link href="#skills">Skills</Nav.Link>
-      <Nav.Link href="#contact">Contact</Nav.Link>
-     
+      <Nav.Link href="#home" className="navBarItem">Home</Nav.Link>
+      <Nav.Link href="#about" className="navBarItem">About Me</Nav.Link>
+      <Nav.Link href="#projects" className="navBarItem">Projects</Nav.Link>
+      <Nav.Link href="#contact" className="navBarItem">Contact</Nav.Link>
     </Nav>
     
   </Navbar.Collapse>

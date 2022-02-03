19099e3c77d0710918279b3829fb419417d6ae1a
Niran S Narayanan <nirans2002@gmail.com>
2022-02-03 14:47:38 +0530

    navbar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -10,7 +10,7 @@ export const NavigationBar = () => {
   <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
-    {/* <Nav className="me-auto">
+    <Nav className="ms-auto">
       <Nav.Link href="#features">Features</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
@@ -20,13 +20,8 @@ export const NavigationBar = () => {
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
-    </Nav> */}
-    <Nav className="justify-content-between">
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
     </Nav>
+    
   </Navbar.Collapse>
   </Container>
 </Navbar>

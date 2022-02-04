34160cbbf5259a7c4c19389dc48e29ba59b84831
Niran S Narayanan <nirans2002@gmail.com>
2022-02-04 14:28:18 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {NavigationBar} from './components/navBar';
 import Header from './components/header';
 import Particle from './components/particles';
-
+import {BrowserRouter as Router,Switch} from 'react-router-dom'
+import { Footer } from './components/footer';
 function App() {
   
   return (
     <div className="App">
-      <Particle />
+      {/* <Particle /> */}
       <NavigationBar/>
       <Header/>
+      <Footer/>
 
     </div>
   );

4d8872a91647ada4015ead6325eb9e2780bb3fdd
Niran S Narayanan <nirans2002@gmail.com>
2022-02-05 13:48:28 +0530

    refractor

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import {NavigationBar} from './components/navBar';
 import Header from './components/header';
 import Particle from './components/particles';
 import {Footer} from './components/footer';
-import Contact from './components/contact';
-import Projects from './components/projects';
+
 import Main from './components/main';
 
 function App() {
   
   return (
     <div className="App">
-      {/* <Particle /> */}
+      <Particle />
       <NavigationBar/>
       <Main/>
-      <Projects/>
-      <Contact/>
       <Footer/>
     </div>
   );
--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,60 @@
-import React, { Component } from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
+// import {Typing} from './components/typing';
+// import Typing from './components/typing';
+import ReactTypingEffect from 'react-typing-effect';
 
-export default class Home extends Component {
-  render() {
-    return <div>
-        <p>home</p>
-    </div>;
-  }
+
+export default function Home() {
+    return (
+        <section id="about">
+            <div className="whiteText container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                        Hi, Niran S Narayanan
+
+                    </h1>
+                    <div className="flex justify-center">
+                        <span><h1>I work on {Typing()}</h1></span>
+                    </div>
+                    <div className="flex justify-center">
+                        
+                       <Button variant="outline-warning" className="buttonMain"   href="#contact">
+                            Connect With Me
+                        
+                        </Button>
+                        <Button variant="outline-warning" className="buttonMain"  href="#projects"> 
+                            See My works
+                        </Button>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+    );
+}
+
+function Typing() {
+    return <ReactTypingEffect
+        text={["Flutter", "Web", "IoT", "Arduino", "React"]}
+        speed={100}
+        eraseSpeed={50}
+        typingDelay={1000}
+        eraseDelay={1000}
+        cursorRenderer={cursor => <h1>{cursor}</h1>}
+        displayTextRenderer={(text, i) => {
+            return (
+                <h1>
+                    {text.split('').map((char, i) => {
+                        const key = `${i}`;
+                        return (
+                            <span
+                                key={key}
+                                style={i % 2 === 0 ? { color: 'orange' } : {}}
+                            >{char}</span>
+                        );
+                    })}
+                </h1>
+            );
+        } } />;
 }
